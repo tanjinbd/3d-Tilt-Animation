188bb8e5c0c01850e6f038b8fb65c366a4251c3a
Tanjin Ahsan <tanjin.h@gmail.com>
2022-09-20 09:07:21 +0600

    Update tiltSlider.js
    Performance improvement /bug fix

--- js/tiltSlider.js
+++ js/tiltSlider.js
@@ -1,13 +1,4 @@
-/**
- * tiltSlider.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2014, Codrops
- * http://www.codrops.com
- */
+
 ;( function( window ) {
 
 	'use strict';
